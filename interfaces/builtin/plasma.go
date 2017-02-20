38c9f6f1e2b44bbc7978982235d7a5613dc686a2
Harald Sitter <sitter@kde.org>
2017-02-20 11:26:29 +0100

    wip

--- interfaces/builtin/plasma.go
+++ interfaces/builtin/plasma.go
@@ -20,242 +20,113 @@
 package builtin
 
 import (
-	"bytes"
 	"fmt"
 
 	"github.com/snapcore/snapd/interfaces"
-	"github.com/snapcore/snapd/release"
-	"github.com/snapcore/snapd/snap"
 )
 
-const plasmaObservePermanentSlotAppArmor = `
-# Description: Allow operating as the UPower service.
+// PlasmaInterface is the hello interface for a tutorial.
+type PlasmaInterface struct{}
 
-network netlink raw,
-
-# DBus accesses
-#include <abstractions/dbus-strict>
-
-dbus (send)
-   bus=system
-   path=/org/freedesktop/DBus
-   interface=org.freedesktop.DBus
-   member={Request,Release}Name
-   peer=(name=org.freedesktop.DBus),
-
-dbus (receive, send)
-   bus=system
-   path=/org/freedesktop/DBus
-   interface=org.freedesktop.DBus
-   member="GetConnectionUnix{ProcessID,User}"
-   peer=(label=unconfined),
-
-# Allow binding the service to the requested connection name
-dbus (bind)
-    bus=system
-    name="org.freedesktop.UPower",
-
-# Allow read-only access to service properties
-dbus (receive)
-    bus=system
-    path=/org/freedesktop/login1{,/**}
-    interface=org.freedesktop.DBus.Properties
-    peer=(label=unconfined),
-dbus (send)
-    bus=system
-    path=/org/freedesktop/login1{,/**}
-    interface=org.freedesktop.DBus.Properties
-    member=Get{,All}
-    peer=(label=unconfined),
-
-# Allow receiving any signals from the logind service
-dbus (receive)
-    bus=system
-    path=/org/freedesktop/login1{,/**}
-    interface=org.freedesktop.login1.*
-    peer=(label=unconfined),
-
-# Allow access to logind service as we need to query it for possible
-# power states and trigger these when the battery gets low and the
-# system enters a critical state.
-dbus (send)
-    bus=system
-    path=/org/freedesktop/login1{,/**}
-    interface=org.freedesktop.login1.Manager
-    member={CanPowerOff,CanSuspend,CanHibernate,CanHybridSleep,PowerOff,Suspend,Hibernate,HybrisSleep}
-    peer=(label=unconfined),
-`
-
-const plasmaObserveConnectedSlotAppArmor = `
-# Allow traffic to/from our path and interface with any method
-dbus (receive, send)
-    bus=system
-    path=/org/freedesktop/UPower{,/**}
-    interface=org.freedesktop.UPower*
-    peer=(label=###PLUG_SECURITY_TAGS###),
-
-# Allow traffic to/from org.freedesktop.DBus for the UPower service
-dbus (receive, send)
-    bus=system
-    path=/org/freedesktop/UPower{,/**}
-    interface=org.freedesktop.DBus.*
-    peer=(label=###PLUG_SECURITY_TAGS###),
-`
-
-const plasmaObservePermanentSlotSeccomp = `
-bind
-`
-
-const plasmaObservePermanentSlotDBus = `
-<!-- DBus policy for plasma (based on upstream version 0.99.4) -->
-<policy user="root">
-  <allow own="org.freedesktop.UPower"/>
-</policy>
-<policy context="default">
-  <deny own="org.freedesktop.UPower"/>
-
-  <allow send_destination="org.freedesktop.UPower"
-         send_interface="org.freedesktop.DBus.Introspectable"/>
-
-  <allow send_destination="org.freedesktop.UPower"
-         send_interface="org.freedesktop.DBus.Peer"/>
-  <allow send_destination="org.freedesktop.UPower"
-         send_interface="org.freedesktop.DBus.Properties"/>
-  <allow send_destination="org.freedesktop.UPower.Device"
-         send_interface="org.freedesktop.DBus.Properties"/>
-  <allow send_destination="org.freedesktop.UPower.KbdBacklight"
-         send_interface="org.freedesktop.DBus.Properties"/>
-  <allow send_destination="org.freedesktop.UPower.Wakeups"
-         send_interface="org.freedesktop.DBus.Properties"/>
-
-  <allow send_destination="org.freedesktop.UPower"
-         send_interface="org.freedesktop.UPower"/>
-  <allow send_destination="org.freedesktop.UPower"
-         send_interface="org.freedesktop.UPower.Device"/>
-  <allow send_destination="org.freedesktop.UPower"
-         send_interface="org.freedesktop.UPower.KbdBacklight"/>
-  <allow send_destination="org.freedesktop.UPower"
-         send_interface="org.freedesktop.UPower.Wakeups"/>
-</policy>
-`
-
-const plasmaObserveConnectedPlugAppArmor = `
-# Description: Can query UPower for power devices, history and statistics.
-
-#include <abstractions/dbus-strict>
-
-# Find all devices monitored by UPower
-dbus (send)
-    bus=system
-    path=/org/freedesktop/UPower
-    interface=org.freedesktop.UPower
-    member=EnumerateDevices
-    peer=(label=###SLOT_SECURITY_TAGS###),
-
-# Read all properties from UPower and devices
-dbus (send)
-    bus=system
-    path=/org/freedesktop/UPower{,/devices/**}
-    interface=org.freedesktop.DBus.Properties
-    member=Get{,All}
-    peer=(label=###SLOT_SECURITY_TAGS###),
-
-dbus (send)
-    bus=system
-    path=/org/freedesktop/UPower/Wakeups
-    interface=org.freedesktop.DBus.Properties
-    member=Get{,All}
-    peer=(label=###SLOT_SECURITY_TAGS###),
-
-dbus (send)
-    bus=system
-    path=/org/freedesktop/UPower
-    interface=org.freedesktop.UPower
-    member=GetCriticalAction
-    peer=(label=###SLOT_SECURITY_TAGS###),
-
-dbus (send)
-    bus=system
-    path=/org/freedesktop/UPower/devices/**
-    interface=org.freedesktop.UPower.Device
-    member=GetHistory
-    peer=(label=###SLOT_SECURITY_TAGS###),
-
-# Receive property changed events
-dbus (receive)
-    bus=system
-    path=/org/freedesktop/UPower{,/devices/**}
-    interface=org.freedesktop.DBus.Properties
-    member=PropertiesChanged
-    peer=(label=###SLOT_SECURITY_TAGS###),
-`
-
-type PlasmaObserveInterface struct{}
-
-func (iface *PlasmaObserveInterface) Name() string {
+// String returns the same value as Name().
+func (iface *PlasmaInterface) Name() string {
 	return "plasma"
 }
 
-func (iface *PlasmaObserveInterface) PermanentPlugSnippet(plug *interfaces.Plug, securitySystem interfaces.SecuritySystem) ([]byte, error) {
-	return nil, nil
-}
-
-func (iface *PlasmaObserveInterface) ConnectedPlugSnippet(plug *interfaces.Plug, slot *interfaces.Slot, securitySystem interfaces.SecuritySystem) ([]byte, error) {
-	switch securitySystem {
-	case interfaces.SecurityAppArmor:
-		old := []byte("###SLOT_SECURITY_TAGS###")
-		new := slotAppLabelExpr(slot)
-		if release.OnClassic {
-			// Let confined apps access unconfined plasma on classic
-			new = []byte("unconfined")
-		}
-		snippet := bytes.Replace([]byte(plasmaObserveConnectedPlugAppArmor), old, new, -1)
-		return snippet, nil
-	}
-	return nil, nil
-}
-
-func (iface *PlasmaObserveInterface) PermanentSlotSnippet(slot *interfaces.Slot, securitySystem interfaces.SecuritySystem) ([]byte, error) {
-	switch securitySystem {
-	case interfaces.SecurityDBus:
-		return []byte(plasmaObservePermanentSlotDBus), nil
-	case interfaces.SecurityAppArmor:
-		return []byte(plasmaObservePermanentSlotAppArmor), nil
-	case interfaces.SecuritySecComp:
-		return []byte(plasmaObservePermanentSlotSeccomp), nil
-	}
-	return nil, nil
-}
-
-func (iface *PlasmaObserveInterface) ConnectedSlotSnippet(plug *interfaces.Plug, slot *interfaces.Slot, securitySystem interfaces.SecuritySystem) ([]byte, error) {
-	switch securitySystem {
-	case interfaces.SecurityAppArmor:
-		old := []byte("###PLUG_SECURITY_TAGS###")
-		new := plugAppLabelExpr(plug)
-		snippet := bytes.Replace([]byte(plasmaObserveConnectedSlotAppArmor), old, new, -1)
-		return snippet, nil
-	}
-	return nil, nil
-}
-
-func (iface *PlasmaObserveInterface) SanitizePlug(plug *interfaces.Plug) error {
-	if iface.Name() != plug.Interface {
-		panic(fmt.Sprintf("plug is not of interface %q", iface.Name()))
-	}
-	return nil
-}
-
-func (iface *PlasmaObserveInterface) SanitizeSlot(slot *interfaces.Slot) error {
+// SanitizeSlot checks and possibly modifies a slot.
+func (iface *PlasmaInterface) SanitizeSlot(slot *interfaces.Slot) error {
 	if iface.Name() != slot.Interface {
-		panic(fmt.Sprintf("slot is not of interface %q", iface.Name()))
+		panic(fmt.Sprintf("slot is not of interface %q", iface))
 	}
-	if slot.Snap.Type != snap.TypeApp && slot.Snap.Type != snap.TypeOS {
-		return fmt.Errorf("%s slots are reserved for the operating system or application snaps", iface.Name())
-	}
+	// NOTE: currently we don't check anything on the slot side.
 	return nil
 }
 
-func (iface *PlasmaObserveInterface) AutoConnect(*interfaces.Plug, *interfaces.Slot) bool {
-	// allow what declarations allowed
-	return true
+// SanitizePlug checks and possibly modifies a plug.
+func (iface *PlasmaInterface) SanitizePlug(plug *interfaces.Plug) error {
+	if iface.Name() != plug.Interface {
+		panic(fmt.Sprintf("plug is not of interface %q", iface))
+	}
+	// NOTE: currently we don't check anything on the plug side.
+	return nil
+}
+
+// ConnectedSlotSnippet returns security snippet specific to a given connection between the hello slot and some plug.
+func (iface *PlasmaInterface) ConnectedSlotSnippet(plug *interfaces.Plug, slot *interfaces.Slot, securitySystem interfaces.SecuritySystem) ([]byte, error) {
+	switch securitySystem {
+	case interfaces.SecurityAppArmor:
+		return nil, nil
+	case interfaces.SecuritySecComp:
+		return nil, nil
+	case interfaces.SecurityDBus:
+		return nil, nil
+	case interfaces.SecurityUDev:
+		return nil, nil
+	case interfaces.SecurityMount:
+		return nil, nil
+	default:
+		return nil, interfaces.ErrUnknownSecurity
+	}
+}
+
+// PermanentSlotSnippet returns security snippet permanently granted to hello slots.
+func (iface *PlasmaInterface) PermanentSlotSnippet(slot *interfaces.Slot, securitySystem interfaces.SecuritySystem) ([]byte, error) {
+	switch securitySystem {
+	case interfaces.SecurityAppArmor:
+		return nil, nil
+	case interfaces.SecuritySecComp:
+		return nil, nil
+	case interfaces.SecurityDBus:
+		return nil, nil
+	case interfaces.SecurityUDev:
+		return nil, nil
+	case interfaces.SecurityMount:
+		return nil, nil
+	default:
+		return nil, interfaces.ErrUnknownSecurity
+	}
+}
+
+// ConnectedPlugSnippet returns security snippet specific to a given connection between the hello plug and some slot.
+func (iface *PlasmaInterface) ConnectedPlugSnippet(plug *interfaces.Plug, slot *interfaces.Slot, securitySystem interfaces.SecuritySystem) ([]byte, error) {
+	switch securitySystem {
+	case interfaces.SecurityAppArmor:
+		return nil, nil
+	case interfaces.SecuritySecComp:
+		return nil, nil
+	case interfaces.SecurityDBus:
+		return nil, nil
+	case interfaces.SecurityUDev:
+		return nil, nil
+	case interfaces.SecurityMount:
+		return nil, nil
+	default:
+		return nil, interfaces.ErrUnknownSecurity
+	}
+}
+
+// PermanentPlugSnippet returns the configuration snippet required to use a hello interface.
+func (iface *PlasmaInterface) PermanentPlugSnippet(plug *interfaces.Plug, securitySystem interfaces.SecuritySystem) ([]byte, error) {
+	switch securitySystem {
+	case interfaces.SecurityAppArmor:
+		return nil, nil
+	case interfaces.SecuritySecComp:
+		return nil, nil
+	case interfaces.SecurityDBus:
+		return nil, nil
+	case interfaces.SecurityUDev:
+		return nil, nil
+	case interfaces.SecurityMount:
+		return nil, nil
+	default:
+		return nil, interfaces.ErrUnknownSecurity
+	}
+}
+
+// AutoConnect returns true if plugs and slots should be implicitly
+// auto-connected when an unambiguous connection candidate is available.
+//
+// This interface does not auto-connect.
+func (iface *PlasmaInterface) AutoConnect() bool {
+	return false
 }
